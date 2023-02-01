b4c08ba0d712522099fccbd7ac0ea07df0f908b5
[AI6/2반] 박재희 <dalkey23@gmail.com>
2023-02-01 13:54:51 +0900

    fix: 목록, 상세페이지 수정

--- src/components/Body/bodyPages/Details.js
+++ src/components/Body/bodyPages/Details.js
@@ -1,5 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+
+const Product1 = {
+    productName : "제품명", categoryId : 1, manufacturer : "제조사", shortDesc : "짧은 설명",
+    detailDesc  : "상세 설명", imgUrl : "이미지", totalstocks : "재고수", price : "가격", 
+    sku : "개수단위"
+}
+
+const Container = styled.div`
+    display : flex;
+    align-items : center;
+    padding : 20px;
+    
+    `
+const ProductImg = styled.div`
+    width : 50%;
+    height : 200px;
+    background : powderblue;
+
+`
+const Wrapper = styled.div`
+    padding : 10px;
+    width : 50%;
+`
+const Items = styled.div`
+    margin : 10px;
+`
+const ButtonWrapper = styled.div`
+    & button {
+        color : white;
+        background : grey;
+        border : grey;
+        width : 45%;
+        margin : 10px;
+    }
+
+`    
+
 const Details = ()=>{
-    return <>details</>
+    return <Container>
+                <ProductImg>{Product1.imgUrl}</ProductImg>
+                <Wrapper>
+                    <Items>{Product1.manufacturer}</Items>
+                    <Items>{Product1.productName}</Items>
+                    <Items>{Product1.price}</Items>
+                    <Items>{Product1.detailDesc}</Items>
+                    <ButtonWrapper>
+                        <button>장바구니 추가하기</button>
+                        <button>바로 구매하기</button>
+                    </ButtonWrapper>
+                </Wrapper>
+            </Container>
 }
 
 export default Details;
--- src/components/Body/bodyPages/Parenting.js
+++ src/components/Body/bodyPages/Parenting.js
@@ -1,34 +1,93 @@
 import React from "react";
 import styled from "styled-components";
 
-
 const ListContainer = styled.div`
     display : flex;
     align-items : center;
+    flex-wrap : wrap;
+    & a {
+        text-decoration : none;
+        color : white;
+        width : 25%;
+        padding : 10px;
+    }
 
 `
 
-const StyledListDiv = styled.div`
+const ListItems = styled.div`
     flex : 1;
     background : gray;
-    width : 200px;
+    width : 95%;
     height : 200px;
     margin : 10px;
     
-    `
+`
+
+const Item = styled.div`
+   
+`
+
+const listItems = [{
+    productName: "육아1", categoryId: 1, manufacturer: "제조사", shortDesc: "짧은 설명",
+    detailDesc: "상세 설명", imgUrl: "이미지", totalstocks: "재고수", price: "가격",
+    sku: "개수단위"
+}, {
+    productName: "육아2", categoryId: 1, manufacturer: "제조사", shortDesc: "짧은 설명",
+    detailDesc: "상세 설명", imgUrl: "이미지", totalstocks: "재고수", price: "가격",
+    sku: "개수단위"
+}, {
+    productName: "육아3", categoryId: 1, manufacturer: "제조사", shortDesc: "짧은 설명",
+    detailDesc: "상세 설명", imgUrl: "이미지", totalstocks: "재고수", price: "가격",
+    sku: "개수단위"
+}, {
+    productName: "육아4", categoryId: 1, manufacturer: "제조사", shortDesc: "짧은 설명",
+    detailDesc: "상세 설명", imgUrl: "이미지", totalstocks: "재고수", price: "가격",
+    sku: "개수단위"
+}, {
+    productName: "육아5", categoryId: 1, manufacturer: "제조사", shortDesc: "짧은 설명",
+    detailDesc: "상세 설명", imgUrl: "이미지", totalstocks: "재고수", price: "가격",
+    sku: "개수단위"
+}, {
+    productName: "육아6", categoryId: 1, manufacturer: "제조사", shortDesc: "짧은 설명",
+    detailDesc: "상세 설명", imgUrl: "이미지", totalstocks: "재고수", price: "가격",
+    sku: "개수단위"
+}, {
+    productName: "육아7", categoryId: 1, manufacturer: "제조사", shortDesc: "짧은 설명",
+    detailDesc: "상세 설명", imgUrl: "이미지", totalstocks: "재고수", price: "가격",
+    sku: "개수단위"
+}, {
+    productName: "육아8", categoryId: 1, manufacturer: "제조사", shortDesc: "짧은 설명",
+    detailDesc: "상세 설명", imgUrl: "이미지", totalstocks: "재고수", price: "가격",
+    sku: "개수단위"
+}, {
+    productName: "육아9", categoryId: 1, manufacturer: "제조사", shortDesc: "짧은 설명",
+    detailDesc: "상세 설명", imgUrl: "이미지", totalstocks: "재고수", price: "가격",
+    sku: "개수단위"
+}];
 
 
 function Parenting() {
-    const listItems = ["출산1", "출산2", "출산3", "출산4", "출산5"];
-      
+
+
     return <ListContainer>
 
-        {listItems.map((listItem, index)=>{
-        return <a href="/details"><StyledListDiv>{listItem}</StyledListDiv></a>
+        {listItems.map((listItem) => {
+            return <a href="/details">
+                        <ListItems>
+                            <Item>{listItem.imgUrl}</Item>
+                            <Item>{listItem.productName}</Item>
+                            <Item>{listItem.shortDesc}</Item>
+                            <Item>{listItem.price}</Item>
+                        </ListItems>
+                    </a>
 
         })}
 
     </ListContainer>
-    }
+}
+
+
+
+
 
 export default Parenting;
