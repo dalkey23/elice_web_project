6ce862d91a5a97320878c6f6f149c96e4bdd3f7a
조인식 <imsif42@gmail.com>
2023-02-01 16:21:03 +0900

    feat: 로그인미들웨어

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { changeUser } from "../services/userService";
+import { changeUser, seeMyPage, logOut } from "../services/userService";
+import { loginRequired } from "../middlewares/loginRequired.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/edit", changeUser);
+userRouter.get("/logout", logOut);
+userRouter.get("/:id", loginRequired, seeMyPage);
+userRouter.post("/edit", loginRequired, changeUser);
 
 export default userRouter;
