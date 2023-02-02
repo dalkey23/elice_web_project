7bed1d040b1beecb900a947d5e76ccc3a3468c19
조인식 <imsif42@gmail.com>
2023-02-03 00:59:57 +0900

    feat: payment db 구축

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { postJoin, postLogin } from "../services/userService";
+import { postOrder } from "../services/paymentService";
 
 const rootRouter = express.Router();
 
 rootRouter.post("/join", postJoin);
 rootRouter.post("/login", postLogin);
+rootRouter.post("/order", postOrder);
 
 export default rootRouter;
---
+++ src/services/paymentService.js
@@ -0,0 +1,6 @@
+import Payment from "../db/models/paymentModel";
+
+//주문한 상품 등록
+export const postOrder = async (req, res) => {
+  res.send("1");
+};
