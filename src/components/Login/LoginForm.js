c351d6276e3e99c8a4f122855230abbd3ec24b6c
조인식 <imsif42@gmail.com>
2023-02-06 23:26:36 +0900

    fix: post login

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import { Link , useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import axios from 'axios';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import axios from "axios";
 
 // 아직 사용 안하는 import 그룹
-import Modal from 'react-bootstrap/Modal';
-import InputGroup from 'react-bootstrap/InputGroup';
+import Modal from "react-bootstrap/Modal";
+import InputGroup from "react-bootstrap/InputGroup";
 
 const LoginForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   // 유효성 검사 로직 보류
@@ -20,34 +20,25 @@ const LoginForm = () => {
 
   // email, password를 보내는 post 요청
   const handleSubmit = (e) => {
-    
     e.preventDefault();
 
-    const formdata = { email, password }
-    
+    const formdata = { email, password };
+
     const onSubmit = () => {
-
       axios
-      .post("http://localhost:8080/login", { ...formdata })
-      .then((response) => {
-      localStorage.setItem('jwt-token', JSON.stringify(response.data))
-      
-      let jwttoken = localStorage.getItem('jwt-token') || '';
+        .post("http://localhost:8080/login", { ...formdata })
+        .then((res) => {
+          localStorage.setItem("accessToken", res.data);
 
-        fetch('http://localhost:3000/', {
-          headers: {
-            'Authorization': jwttoken,
-          }
+          axios.defaults.headers.common["Authorization"] = res.data;
         })
-        .then(navigate("/"))
-      })
-      .catch((err) => {
-        alert(err)
-      })
-      }
+        .catch((err) => {
+          alert(err);
+        });
+    };
 
     onSubmit();
-    }
+  };
 
   // const handleSubmitCorrect = (event) => {
   //   const form = event.currentTarget;
@@ -57,60 +48,66 @@ const LoginForm = () => {
   //   }
   //   setValidated(true);
   // };
-  
+
   return (
-    <div style = {{
-      display : 'flex',
-      justifyContent : 'center'
-    }}>
-    <Login>
-      <FloatingLabel
-        controlId="floatingInput"
-        label="이메일"
-        className="mb-3"
-        onChange={ (e) => setEmail(e.target.value)}>
-        <Form.Control type="email"/>
-      </FloatingLabel>
-      <FloatingLabel
-        controlId="floatingPassword"
-        label="비밀번호"
-        onChange={ (e) => setPassword(e.target.value)}>
-        <Form.Control type="password"/>
-      </FloatingLabel>
-    <div style = {{
-      display : 'flex',
-      justifyContent : 'flex-end',
-      padding : '20px'
-    }}>
-      <Button type = 'submit' style = {{
-        borderRadius : '5px',
-        backgroundColor : 'grey',
-        borderColor : 'grey'
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
       }}
-        onClick={ handleSubmit } >
-          로그인
-      </Button>
-      <Button style = {{
-        marginLeft : '5px',
-        borderRadius : '5px',
-        backgroundColor : 'grey',
-        borderColor : 'grey'
-      }}>
-        <Link to='/RegisterForm' style = {{
-              textDecoration : 'none',
-              color : 'white'
-              }}>회원가입
-        </Link>
-      </Button>
+    >
+      <Login>
+        <FloatingLabel controlId="floatingInput" label="이메일" className="mb-3" onChange={(e) => setEmail(e.target.value)}>
+          <Form.Control type="email" />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingPassword" label="비밀번호" onChange={(e) => setPassword(e.target.value)}>
+          <Form.Control type="password" />
+        </FloatingLabel>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            padding: "20px",
+          }}
+        >
+          <Button
+            type="submit"
+            style={{
+              borderRadius: "5px",
+              backgroundColor: "grey",
+              borderColor: "grey",
+            }}
+            onClick={handleSubmit}
+          >
+            로그인
+          </Button>
+          <Button
+            style={{
+              marginLeft: "5px",
+              borderRadius: "5px",
+              backgroundColor: "grey",
+              borderColor: "grey",
+            }}
+          >
+            <Link
+              to="/RegisterForm"
+              style={{
+                textDecoration: "none",
+                color: "white",
+              }}
+            >
+              회원가입
+            </Link>
+          </Button>
+        </div>
+      </Login>
     </div>
-    </Login>
-  </div>
   );
-}
+};
 
 const Login = styled.div`
-    padding : 300px;
-    width : 60%;
-`
+  padding: 300px;
+  width: 60%;
+`;
 
 export default LoginForm;
--- src/components/User/UserInfo.js
+++ src/components/User/UserInfo.js
@@ -1,40 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import { NavLink , useNavigate } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 const UserInfo = () => {
-    const navigate = useNavigate();
-    const [data, setData] = useState('');
-    useEffect(() => {
-      axios
-      .get("http://localhost:8080/users/27")
+  const navigate = useNavigate();
+  const [data, setData] = useState("");
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/users/mypage")
       .then((response) => {
-        setData(response.data)
+        setData(response.data);
       })
       .catch((error) => {
-        alert(error)
-      })
-    }, [])
-  
+        alert(error);
+      });
+  }, []);
+
   return (
-    <div style = {{
-      margin : '30px'
-    }}>
-      <h1 style = {{
-          display : 'flex',
-          justifyContent : 'center',
-          padding : '10px'
-        }}>{`${JSON.stringify(data.name)}님 환영합니다!`}</h1>
-      <div style = {{
-      display : 'flex',
-      justifyContent : 'center'
-      }}>
-        <div style = {{
-      height : '50%',
-      padding : '100px'
-      }}>
+    <div
+      style={{
+        margin: "30px",
+      }}
+    >
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          padding: "10px",
+        }}
+      >{`${JSON.stringify(data.name)}님 환영합니다!`}</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            height: "50%",
+            padding: "100px",
+          }}
+        >
           <Card className="mb-5">
             <Card.Body>이메일 : {JSON.stringify(data.email)}</Card.Body>
           </Card>
@@ -45,50 +53,65 @@ const UserInfo = () => {
             <Card.Body>주소 : {JSON.stringify(data.address)}</Card.Body>
           </Card>
           {/* 회원 정보 수정 탈퇴 버튼 */}
-          <div style = {{
-                padding : '5px',
-                margin : '20px'
-              }}>
-                <NavLink to="/UserUpdate" style = {{
-                textDecorationLine : 'none'
-            }}>
-                <button style = {{
-                    padding : '8px',
-                    borderRadius : '8px',
-                    borderColor : 'white',
-                    backgroundColor : 'grey',
-                    color : 'white'
-                }}>정보 수정</button>
+          <div
+            style={{
+              padding: "5px",
+              margin: "20px",
+            }}
+          >
+            <NavLink
+              to="/UserUpdate"
+              style={{
+                textDecorationLine: "none",
+              }}
+            >
+              <button
+                style={{
+                  padding: "8px",
+                  borderRadius: "8px",
+                  borderColor: "white",
+                  backgroundColor: "grey",
+                  color: "white",
+                }}
+              >
+                정보 수정
+              </button>
             </NavLink>
-              <NavLink to="/" style = {{
-                textDecorationLine : 'none',
-                margin : '10px'
-            }}>
-                <button 
-                onClick = {() => {
+            <NavLink
+              to="/"
+              style={{
+                textDecorationLine: "none",
+                margin: "10px",
+              }}
+            >
+              <button
+                onClick={() => {
                   axios
                     .delete("http://localhost:8080/users/21/delete")
                     .then(() => {
-                      alert('회원 탈퇴 되었습니다.')
-                      navigate("/")
+                      alert("회원 탈퇴 되었습니다.");
+                      navigate("/");
                     })
                     .catch((err) => {
-                      alert(err)
-                    })
+                      alert(err);
+                    });
                 }}
-                style = {{
-                    padding : '8px',
-                    borderRadius : '8px',
-                    borderColor : 'white',
-                    backgroundColor : 'red',
-                    color : 'white'
-                }}>회원 탈퇴</button>
+                style={{
+                  padding: "8px",
+                  borderRadius: "8px",
+                  borderColor: "white",
+                  backgroundColor: "red",
+                  color: "white",
+                }}
+              >
+                회원 탈퇴
+              </button>
             </NavLink>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default UserInfo;
