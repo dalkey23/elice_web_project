16085a98e22e93f2f0e3734412c3759c8310b8fc
paul950104@gmail.com <paul950104@gmail.com>
2023-02-07 00:16:51 +0900

    fix: 로그인 최종 구현 완성

--- src/components/User/UserMain.js
+++ src/components/User/UserMain.js
@@ -50,7 +50,7 @@ const UserMain = () => {
                 }}>
                     <button 
                     onClick = {() => {
-                        localStorage.removeItem('jwt-token')
+                        localStorage.removeItem('accessToken')
                         alert('로그아웃 되었습니다.')
                         navigate('/')
                     }}
