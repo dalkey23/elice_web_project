977c73b91c11072f19ffa8d9833e75c903f31a05
조인식 <imsif42@gmail.com>
2023-02-07 17:34:12 +0900

    fix: 회원탈퇴

--- src/components/User/UserDelete.js
+++ src/components/User/UserDelete.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import axios from 'axios';
-import styled from 'styled-components';
+import axios from "axios";
+import styled from "styled-components";
 
 const UserDelete = () => {
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
-
-    const handleSubmit = (e) => {
-      e.preventDefault();
-
-      const onSubmit = () => {
-        axios
-        .delete("http://localhost:8080/users/delete/:userId")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = { password };
+    const onSubmit = () => {
+      axios
+        .post("http://localhost:8080/users/delete/:userId", { ...formData })
         .then(() => {
           alert("회원 탈퇴 되었습니다.");
           navigate("/");
@@ -24,47 +23,49 @@ const UserDelete = () => {
         .catch((err) => {
           alert(err);
         });
-      }
-      onSubmit();
-    }
+    };
+    onSubmit();
+  };
 
-
-    return (
-        <div style = {{
-            margin : '20%'
-        }}>
-        <Delete>
-        <FloatingLabel controlId="floatingPassword" label="비밀번호"
-        value = { password } onChange={(e) => setPassword(e.target.value)}>
+  return (
+    <div
+      style={{
+        margin: "20%",
+      }}
+    >
+      <Delete>
+        <FloatingLabel controlId="floatingPassword" label="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)}>
           <Form.Control type="password" />
         </FloatingLabel>
-        <div style = {{
-            margin : '10px',
-            display : 'flex',
-            justifyContent : 'center'
-        }}>
-        <Button
-          type = 'submit'
-          onClick={ handleSubmit }
+        <div
           style={{
-            padding: "8px",
-            borderRadius: "8px",
-            borderColor: "white",
-            backgroundColor: "red",
-            color: "white",
+            margin: "10px",
+            display: "flex",
+            justifyContent: "center",
           }}
         >
-          회원 탈퇴
-        </Button>
+          <Button
+            type="submit"
+            onClick={handleSubmit}
+            style={{
+              padding: "8px",
+              borderRadius: "8px",
+              borderColor: "white",
+              backgroundColor: "red",
+              color: "white",
+            }}
+          >
+            회원 탈퇴
+          </Button>
         </div>
-        </Delete>
-      </div>
-    )
-}
+      </Delete>
+    </div>
+  );
+};
 
 const Delete = styled.div`
-    display : flex;
-    justify-content : center;
-`
+  display: flex;
+  justify-content: center;
+`;
 
 export default UserDelete;
