a99b799e63679243a82fefa021e9112016793fbe
조인식 <imsif42@gmail.com>
2023-02-08 17:45:04 +0900

    fix

--- src/components/User/UserUpdate.js
+++ src/components/User/UserUpdate.js
@@ -1,140 +1,137 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import styled from 'styled-components';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
+import styled from "styled-components";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
 
 const UserUpdate = () => {
+  const [validated, setValidated] = useState(false);
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [data, setData] = useState("");
+  const token = localStorage.getItem("accessToken");
+  const navigate = useNavigate();
 
-    const [validated, setValidated] = useState(false);
-    const [email, setEmail] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [address, setAddress] = useState('');
-    const [data, setData] = useState('')
-    const token = localStorage.getItem("accessToken");
-    const navigate = useNavigate();
-
-    const handleUpdSubmit = (e) => {
-    
+  const handleUpdSubmit = (e) => {
     // 새로고침 방지
     e.preventDefault();
-    
+
     const formUpdData = {
-    email,
-    phoneNumber,
-    address
-    }
+      email,
+      phoneNumber,
+      address,
+    };
     const onUpdSubmit = () => {
-    
-    axios
       // formUpdData로 묶은 값을 구조분해해서 전달
       // post로 회원 정보 변경
-      .get("http://localhost:8080/users/mypage", { headers: { Authorization: token } })
-      .then(() => {
-        axios.post("http://localhost:8080/users/edit/:userId", { ...formUpdData })
-        alert('회원 정보가 수정되었습니다.')
-        navigate("/UserInfo")
-      })
-      .catch((error) => {
-        alert(error)
-      })
-    }
-    
+
+      axios.post("http://localhost:8080/users/edit/:userId", { ...formUpdData }, { headers: { Authorization: token } });
+      alert("회원 정보가 수정되었습니다.");
+      navigate("/UserInfo");
+    };
+
     onUpdSubmit();
-  }
-    useEffect(() => {
-      axios
+  };
+  useEffect(() => {
+    axios
       .get("http://localhost:8080/users/mypage", { headers: { Authorization: token } })
       .then((response) => {
-        setData(response.data)
+        setData(response.data);
       })
       .catch((error) => {
-        alert(error)
-      })
-    }, [])
+        alert(error);
+      });
+  }, []);
 
-      // 유효성 검사하는 로직인데 onSubmit에 2개의 함수를 넣어야 해서 보류
-      // const handleSubmitCorrect = (event) => {
-      //   const form = event.currentTarget;
-      //   if (form.checkValidity() === false) {
-      //     event.preventDefault();
-      //     event.stopPropagation();
-      //   }
-      //   setValidated(true);
+  // 유효성 검사하는 로직인데 onSubmit에 2개의 함수를 넣어야 해서 보류
+  // const handleSubmitCorrect = (event) => {
+  //   const form = event.currentTarget;
+  //   if (form.checkValidity() === false) {
+  //     event.preventDefault();
+  //     event.stopPropagation();
+  //   }
+  //   setValidated(true);
 
-      // 값이 적절하지 않은 경우 빨간 테두리 (Form.Control.Feedback)
-      
-      return (
-        <Update>
-          <Form noValidate validated={validated} style = {{ width : 600 }}>
-            {/* 아이디 */}
-            <Row className="mb-3" >
-              <Form.Group as={Col} md="12" controlId="validationCustom02">
-                <Form.Label>아이디</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder= {JSON.stringify(data.email)}
-                  defaultValue=""
-                  value= { email }
-                  onChange={ (e) => setEmail(e.target.value)}
-                  />
-                <Form.Control.Feedback></Form.Control.Feedback>
-              </Form.Group> 
-            </Row>
-            {/* 전화번호 */}
-              <Row className="mb-3">
-              <Form.Group as={Col} controlId="validationCustom02">
-                <Form.Label>전화번호</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder = {JSON.stringify(data.phoneNumber)}
-                  defaultValue = ""
-                  value={ phoneNumber }
-                  onChange={ (e) => setPhoneNumber(e.target.value)}
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              </Form.Group>
-              </Row>
-              {/* 주소 */}
-              <Row className="mb-3">
-              <Form.Group as={Col} controlId="validationCustom02">
-                <Form.Label>주소</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder = {JSON.stringify(data.address)}
-                  defaultValue=""
-                  value={ address }
-                  onChange={ (e) => setAddress(e.target.value)}
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              </Form.Group>
-              </Row>
-              <BtnDiv>
-                <Button type="submit" style = {{
-                  backgroundColor : 'grey',
-                  border : 'grey',
-                }} onClick = { handleUpdSubmit }>정보 수정</Button>
-            </BtnDiv>
-          </Form>
-          </Update>
-        );
-}
+  // 값이 적절하지 않은 경우 빨간 테두리 (Form.Control.Feedback)
+
+  return (
+    <Update>
+      <Form noValidate validated={validated} style={{ width: 600 }}>
+        {/* 아이디 */}
+        <Row className="mb-3">
+          <Form.Group as={Col} md="12" controlId="validationCustom02">
+            <Form.Label>아이디</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder={JSON.stringify(data.email)}
+              defaultValue=""
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Control.Feedback></Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        {/* 전화번호 */}
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="validationCustom02">
+            <Form.Label>전화번호</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder={JSON.stringify(data.phoneNumber)}
+              defaultValue=""
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        {/* 주소 */}
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="validationCustom02">
+            <Form.Label>주소</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder={JSON.stringify(data.address)}
+              defaultValue=""
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <BtnDiv>
+          <Button
+            type="submit"
+            style={{
+              backgroundColor: "grey",
+              border: "grey",
+            }}
+            onClick={handleUpdSubmit}
+          >
+            정보 수정
+          </Button>
+        </BtnDiv>
+      </Form>
+    </Update>
+  );
+};
 
 const Update = styled.div`
-    display : flex;
-    justify-content : space-around;
-    padding : 150px;
-`
+  display: flex;
+  justify-content: space-around;
+  padding: 150px;
+`;
 
 const BtnDiv = styled.div`
-    display : flex;
-    justify-content : flex-end;
-`
+  display: flex;
+  justify-content: flex-end;
+`;
 
 export default UserUpdate;
