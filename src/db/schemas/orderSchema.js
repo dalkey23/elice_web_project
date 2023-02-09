59adc53e6b63daa323b04b1d6d645cdac1b584b7
mumunuu <jae_won@icloud.com>
2023-02-09 19:26:25 +0900

    fix: 오더스키마 변경

--- src/db/schemas/orderSchema.js
+++ src/db/schemas/orderSchema.js
@@ -1,47 +1,46 @@
-const mongoose = require("mongoose");
-
+import { Schema, mongoose } from "mongoose";
 
 const autoIncrement = require("mongoose-sequence")(mongoose);
-const OrderSchema = new mongoose.Schema(
-  {
-    user: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
-    },
-    name: { type: String },
-    phoneNumber: { type: String },
-    address: { type: String },
-    requirement: { type: String },
-    orderDate: {
-      type: String,
-    },
-
-    products: [
-      {
-        productName: {
-          type: String,
-          required: true,
+const OrderSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: "User",
         },
-        quantity: {
-          type: Number,
-          required: true,
+        name: { type: String },
+        phoneNumber: { type: String },
+        address: { type: String },
+        requirement: { type: String },
+        orderDate: {
+            type: String,
         },
-      },
-    ],
 
-    total: { type: Number },
-    date: { type: Date, default: Date.now() },
-    orderStatus: {
-      type: String,
-      required: true,
-      default: "상품준비중",
+        products: [
+            {
+                productName: {
+                    type: String,
+                    required: true,
+                },
+                quantity: {
+                    type: Number,
+                    required: true,
+                },
+            },
+        ],
+
+        total: { type: Number },
+        date: { type: Date, default: Date.now() },
+        orderStatus: {
+            type: String,
+            required: true,
+            default: "상품준비중",
+        },
     },
-  },
-  {
-    collection: "orders",
-    timestamps: true,
-  }
+    {
+        collection: "orders",
+        timestamps: true,
+    }
 );
 OrderSchema.plugin(autoIncrement, { inc_field: "orderId" });
-export {OrderSchema};
+export  {OrderSchema};
