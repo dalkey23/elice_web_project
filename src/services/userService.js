9a0878b279868b6cc9f9456817456c41af42b553
조인식 <imsif42@gmail.com>
2023-02-09 23:14:05 +0900

    fix: login

--- src/services/userService.js
+++ src/services/userService.js
@@ -34,29 +34,28 @@ export const postJoin = async (req, res) => {
 export const postLogin = async (req, res) => {
   const { email, password } = req.body;
 
-  // //DB에 있는 사용자 인지 확인
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw new Error("해당 이메일은 가입한 사용자가 아닙니다.");
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(403).send({ message: "해당 이메일은 가입 내역이 없습니다. 다시 한 번 확인해 주세요." });
+    }
+    // //비밀번호 확인
+    // //1번째는 프론트에서 가져온 비밀번호, 2번째는 db비밀번호
+    const comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return res.status(403).send({ message: "비밀번호가 일치하지 않습니다." });
+    }
+
+    // 로그인 성공 -> JWT 웹 토큰 생성
+    const secretKey = process.env.JWT_SECRET_KEY || "secret-key";
+    // jwt 토큰에 유저 아이디 담기
+    const token = jwt.sign({ userId: user.userId, role: user.role }, secretKey);
+    const role = user.role;
+    res.json({ token, role });
+    return { token };
+  } catch (error) {
+    return res.status(400).send({ message: "로그인 실패" });
   }
-
-  // //비밀번호 확인
-  // //1번째는 프론트에서 가져온 비밀번호, 2번째는 db비밀번호
-  const comparePassword = bcrypt.compareSync(password, user.password);
-
-  if (!comparePassword) {
-    throw new Error("비밀번호가 일치하지 않습니다.");
-  }
-  // 로그인 성공 -> JWT 웹 토큰 생성
-  const secretKey = process.env.JWT_SECRET_KEY || "secret-key";
-
-  // jwt 토큰에 유저 아이디 담기
-  const token = jwt.sign({ userId: user.userId, role: user.role }, secretKey);
-  const role = user.role;
-
-  res.json({ token, role });
-
-  return { token };
 };
 
 //마이 페이지
