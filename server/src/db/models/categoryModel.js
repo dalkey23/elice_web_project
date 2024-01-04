45c0dd13c3e1dd37dce5c8ef2a7ecac1c189f4cf
[AI6/2반] 박재희 <dalkey23@gmail.com>
2024-01-04 15:50:10 +0900

    fix : es6 적용 db/model 폴더 import 수정

--- server/src/db/models/categoryModel.js
+++ server/src/db/models/categoryModel.js
@@ -1,5 +1,5 @@
 import { model } from "mongoose";
-import { CategorySchema } from "../schemas/categorySchema";
+import { CategorySchema } from "../schemas/categorySchema.js";
 
 const Category = model("Categories", CategorySchema);
 
