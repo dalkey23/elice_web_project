49a8ddc32a708497e14bb0c2fbd2d8d6cabe01cc
[AI6/2반] 박재희 <dalkey23@gmail.com>
2024-01-25 16:59:16 +0900

    fix: 새로고침 시 atom 초기화되는 현상 해결
    atom effects 중 localStorageEffect를 사용하여
    localStorage에 userRole 저장하여 atom 값 초기화되는 현상 해결

--- client/src/recoil/userAtom.js
+++ client/src/recoil/userAtom.js
@@ -1,7 +1,25 @@
 import { atom } from "recoil";
 
 
+const localStorageEffect = key => ({setSelf, onSet}) => {
+    const savedValue = localStorage.getItem(key)
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+  
+    onSet((newValue, _, isReset) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
 export const userAtom = atom({
     key: "userAtom",
     default: "",
+    effects: [
+        localStorageEffect('userRole'),
+      ],
 });
+
+
