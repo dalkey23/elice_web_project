d420fdf0c5f0b0c38d0525c8c6b165cd30527596
[AI6/2반] 박재희 <dalkey23@gmail.com>
2024-02-19 17:54:16 +0900

    fix: 관리자 로그인 시 헤더 변경
    관리자 로그인 시 userRole을 확인하여
    유저로그인과 다른 헤더가 나오도록 수정

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,29 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
-import { getCategories } from "../../apis/products.js";
 import { userAtom } from "../../recoil/userAtom.js";
-import * as SC from "../../styles/Header.js";
 import logo from "../../assets/logo.png";
-import Members from "./Members.jsx";
-import NonMembers from "./NonMembers.jsx";
+import AdminHeader from "./AdminHeader.jsx";
+import UserHeader from "./UserHeader.jsx";
+import * as SC from "../../styles/Header.js";
 
 const Header = () => {
-    const Token = localStorage.getItem("accessToken");
-    const AdminToken = localStorage.getItem("adminToken");
-    const [categories, setCategories] = useState([]);
     const userRole = useRecoilValue(userAtom);
-
-    useEffect(() => {
-        getCategories()
-            .then((response) => {
-                setCategories(response.data.searchAll);
-            })
-            .catch((error) => {
-                alert(error);
-            });
-    }, []);
 
     return (
         <SC.Container>
@@ -32,52 +18,7 @@ const Header = () => {
                     <img src={logo} alt="Logo" />
                 </NavLink>
             </SC.LogoDiv>
-            <SC.NavUl>
-                {" "}
-                {categories.map((category) => {
-                    return (
-                        <li key={category.categoryId}>
-                            {" "}
-                            {AdminToken || !AdminToken === "null" ? (
-                                <> </>
-                            ) : (
-                                <NavLink
-                                    to={`/categories/${category.categoryId}`}>
-                                    {category.name}
-                                </NavLink>
-                            )}
-                        </li>
-                    );
-                })}
-                {/* AdminToken이 admin값일때 관리자페이지 노출 */}
-                {/* <></>이 아닌 다른 방법으로 노출을 조정할 수 있을지 고민 */}
-                <li>
-                    {AdminToken || !AdminToken === "null" ? (
-                        <NavLink to="/AdminMain">관리자페이지</NavLink>
-                    ) : (
-                        <></>
-                    )}
-                </li>
-            </SC.NavUl>
-            <SC.IconUl>
-                {userRole === "user" && <Members />}
-                {userRole === "" && <NonMembers />}
-                
-                <li>
-                    {AdminToken || !AdminToken === "null" ? (
-                        <SC.LogoutButton
-                            onClick={() => {
-                                localStorage.removeItem("adminToken");
-                                alert("관리자 로그아웃 되었습니다.");
-                                window.location.href = "/";
-                            }}>
-                            관리자 로그아웃
-                        </SC.LogoutButton>
-                    ) : (
-                        <></>
-                    )}
-                </li>
-            </SC.IconUl>
+            {userRole === "admin" ? <AdminHeader /> : <UserHeader />}
         </SC.Container>
     );
 };
